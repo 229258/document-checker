f7af6e42fbe146c53c9e8a102459bb7a7b05cf9d
Justyna Owczarek <jus-owczarek@wp.pl>
2022-06-27 22:33:29 +0200

    Add togglable mobile menu

--- frontend/components/Navbar/Navbar.js
+++ frontend/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -19,39 +19,60 @@ const NavbarItem = (props) => {
   );
 };
 
+const NavList = (props) => {
+  const { page } = props;
+
+  return (
+    <div className={styles.navList}>
+      <NavbarItem
+        urlPath={"/information"}
+        page={page}
+        navbarPage={"INFORMATION"}
+        description={"informacje"}
+      />
+
+      <NavbarItem
+        urlPath={"/distinguishing"}
+        page={page}
+        navbarPage={"DISTINGUISHING"}
+        description={"rozpoznawanie"}
+      />
+
+      <NavbarItem
+        urlPath={"/contact"}
+        page={page}
+        navbarPage={"CONTACT"}
+        description={"kontakt"}
+      />
+    </div>
+  )
+}
+
 const Navbar = (props) => {
   const { page } = props;
   const isMobile = useMediaQuery(1200);
+  const [isNavActive, setIsNavActive] = useState(false);
 
   return (
     <div className={styles.container}>
       <div className={styles.nav}>
-        <Link href="/">
-          <Image className={styles.logo} src={isMobile ? '/logo-black.svg' : '/logo-white.svg'} alt="Document checker" height="40" width="100%" />
-        </Link>
-
-        <div className={styles.navList}>
-          <NavbarItem
-            urlPath={"/information"}
-            page={page}
-            navbarPage={"INFORMATION"}
-            description={"informacje"}
-          />
-
-          <NavbarItem
-            urlPath={"/distinguishing"}
-            page={page}
-            navbarPage={"DISTINGUISHING"}
-            description={"rozpoznawanie"}
-          />
-
-          <NavbarItem
-            urlPath={"/contact"}
-            page={page}
-            navbarPage={"CONTACT"}
-            description={"kontakt"}
-          />
+        <div className={`${styles.logoContainer} ${isNavActive ? styles.navActive : null}`}>
+          <Link href="/">
+            <Image className={styles.logo} src={isMobile && isNavActive ? '/logo-black.svg' : '/logo-white.svg'} alt="Document checker" height="40" width={isMobile ? 200 : 240} />
+          </Link>
+          {isMobile &&
+            <Image
+              className={styles.menuIcon}
+              src='/icons/menu.svg'
+              width="30"
+              height="30"
+              alt="Menu"
+              onClick={() => setIsNavActive(!isNavActive)}
+            />
+          }
         </div>
+
+        {!isMobile || isNavActive && <NavList page={page} />}
       </div>
     </div>
   );
