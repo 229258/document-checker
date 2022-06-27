1e66e9680c3d1b5f6830f3da395cdacb4a814301
Justyna Owczarek <jus-owczarek@wp.pl>
2022-06-27 21:43:02 +0200

    Add useMediaQuery hook

---
+++ frontend/hooks/useMediaQuery.hook.js
@@ -0,0 +1,32 @@
+import { useState, useCallback, useEffect } from 'react';
+
+/**
+ * @author https://github.com/vercel/next.js/discussions/14810
+ */
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback((e) => {
+        if (e.matches) {
+            setTargetReached(true);
+        } else {
+            setTargetReached(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`)
+        media.addEventListener('change', e => updateTarget(e))
+
+        // Check on mount (callback is not called until a change occurs)
+        if (media.matches) {
+            setTargetReached(true)
+        }
+
+        return () => media.removeEventListener('change', e => updateTarget(e))
+    }, [])
+
+    return targetReached;
+};
+
+export default useMediaQuery;
