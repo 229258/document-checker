a66ebc0d9bf704944fa20b3b1f2259ab12495e0c
Justyna Owczarek <jus-owczarek@wp.pl>
2022-06-29 18:22:00 +0200

    Remove some old unusued code of globe

--- frontend/components/Layout/Fullscreen.js
+++ frontend/components/Layout/Fullscreen.js
@@ -7,7 +7,7 @@ import styles from "./styles.module.scss";
 import * as globalStyles from "../../styles/globals.module.scss";
 
 const Fullscreen = (props) => {
-  const { component, secondComponent, page } = props;
+  const { component, page } = props;
   return (
     <div className={`${styles.fullscreen} ${globalStyles.mainContainer}`}>
       <Head>
@@ -16,9 +16,6 @@ const Fullscreen = (props) => {
       <Navbar page={page} />
       {/* <div className={styles.break}></div> TODO: remove this & grid beloew after properly griding landing page*/}
       <div style={{ display: "grid", gridColumn: "span 12" }}>
-        <div className={styles.globeColors}>
-          <div className={styles.globe}>{secondComponent}</div>
-        </div>
         {component}
       </div>
     </div>
