dac69be0a5172fa4643e7c71c8cd9ae00b2f3867
Justyna Owczarek <jus-owczarek@wp.pl>
2022-06-29 20:42:04 +0200

    Create proper grid layout for landing page for tablet and mobile

--- frontend/views/Landing/Landing.js
+++ frontend/views/Landing/Landing.js
@@ -5,17 +5,36 @@ import Fullscreen from "../../components/Layout/Fullscreen";
 import LandingStyles from "./Landing.module.scss";
 import Globe from "../../components/Globe";
 import DefaultButton from "../../components/Buttons/DefaultButton";
+import useMediaQuery from "hooks/useMediaQuery.hook";
 
 const Identification = () => {
+  const isMobile = useMediaQuery(1200);
+
   return (
     <>
-      <div className={LandingStyles.break}></div>
+      {!isMobile && <div className={LandingStyles.break}></div>}
       <div className={LandingStyles.textContainer}>
         <h2 className={LandingStyles.h1Light}>
           Identyfikacja dowodów osobistych
         </h2>
         <h1 className={LandingStyles.h1Bold}>w 3 krokach</h1>
 
+        {!isMobile && (
+          <div className={LandingStyles.buttonContainer}>
+            <DefaultButton
+              description={"rozpoznaj kraj"}
+              link={"/distinguishing"}
+              style={"primaryBig"}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={LandingStyles.globeContainer}>
+        <Globe />
+      </div>
+
+      {isMobile && (
         <div className={LandingStyles.buttonContainer}>
           <DefaultButton
             description={"rozpoznaj kraj"}
@@ -23,11 +42,7 @@ const Identification = () => {
             style={"primaryBig"}
           />
         </div>
-      </div>
-
-      <div className={LandingStyles.globeContainer}>
-        <Globe />
-      </div>
+      )}
     </>
   );
 };
