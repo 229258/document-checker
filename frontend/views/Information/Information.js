489bf86b654b5f8ca63bdcf4ff8be0a7bf92341e
Justyna Owczarek <jus-owczarek@wp.pl>
2022-09-13 19:18:21 +0200

    Add react-simple-maps package

--- frontend/views/Information/Information.js
+++ frontend/views/Information/Information.js
@@ -1,15 +1,198 @@
 import React from "react";
 import Main from "../../components/Main";
 import Fullscreen from "../../components/Layout/Fullscreen";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 import InformationStyles from "./Information.module.scss";
 import Success from "components/Success";
 
+const allCountries = [
+  'Albania',
+  'Aland',
+  'Andorra',
+  'Austria',
+  'BrusselsCapitalRegion',
+  'FlemishRegion',
+  'Bulgaria',
+  'BosniaandHerzegovina',
+  'RepublicSrpska',
+  'Belarus',
+  'WalloonRegion',
+  'Switzerland',
+  'CzechRepublic',
+  'Germany',
+  'Denmark',
+  'England',
+  'Estonia',
+  'Spain',
+  'Finland',
+  'FaroeIslands',
+  'France',
+  'Guernsey',
+  'Gibraltar',
+  'Greece',
+  'Croatia',
+  'Hungary',
+  'IsleofMan',
+  'Ireland',
+  'Iceland',
+  'Italy',
+  'Jersey',
+  'Kosovo',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Latvia',
+  'Monaco',
+  'Moldova',
+  'Macedonia',
+  'Malta',
+  'Montenegro',
+  'NorthernIreland',
+  'JanMayen',
+  'Netherlands',
+  'Norway',
+  'Svalbard',
+  'Poland',
+  'Portugal',
+  'Romania',
+  'Russia',
+  'Scotland',
+  'SanMarino',
+  'Serbia',
+  'Vojvodina',
+  'Slovakia',
+  'Slovenia',
+  'Sweden',
+  'Ukraine',
+  'Vatican',
+  'Wales'
+];
+
+const countriesAvailable = {
+  'Poland': { name: 'Polska', code: 'POL' },
+}
+
+const Country = ({ geo, type, handleClick }) => {
+  switch (type) {
+    case 'SELECTED':
+      return (
+        <Geography
+          key={geo.rsmKey}
+          geography={geo}
+          stroke="#EAEAEC"
+          style={{
+            default: {
+              fill: "#FBDB6C",
+              outline: "none",
+            },
+            hover: {
+              fill: "#FAD247",
+              outline: "none",
+              cursor: "pointer"
+            },
+            pressed: {
+              fill: "#FBDB6C",
+              outline: "none",
+              cursor: "pointer"
+            }
+          }}
+        />
+      );
+
+    case 'AVAILABLE':
+      return (
+        <Geography
+          key={geo.rsmKey}
+          geography={geo}
+          stroke="#EAEAEC"
+          onClick={() => handleClick(geo)}
+          style={{
+            default: {
+              fill: "#BDBDC4",
+              outline: "none",
+            },
+            hover: {
+              fill: "#FAD247",
+              outline: "none",
+              cursor: "pointer"
+            },
+            pressed: {
+              fill: "#FBDB6C",
+              outline: "none",
+              cursor: "pointer"
+            }
+          }}
+        />
+      );
+
+    case 'UNAVAILABLE':
+    default:
+      return (
+        <Geography
+          key={geo.rsmKey}
+          geography={geo}
+          stroke="#EAEAEC"
+          style={{
+            default: {
+              fill: "#9998A3",
+              outline: "none"
+            },
+            hover: {
+              fill: "#9998A3",
+              outline: "none"
+            },
+            pressed: {
+              fill: "#9998A3",
+              outline: "none"
+            }
+          }}
+        />
+      );
+
+  }
+
+}
+
 const Map = () => {
+  const geoUrl =
+    "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/europe.json";
+  const [selectedCountry, setSelectedCountry] = React.useState('Poland');
+
+  const handleClick = (geo) => {
+    console.log(geo);
+  }
+
   return (
     <div className={InformationStyles.mapa}>
-      Klikalna mapa świata
-    </div>
+      <ComposableMap
+        width={800}
+        height={800}
+        projection="geoAzimuthalEqualArea"
+        projectionConfig={{
+          rotate: [-10.0, -53.0, 0],
+          scale: 1200
+        }}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const country = geo.properties.geounit;
+
+              if (country === selectedCountry) {
+                return <Country geo={geo} type='SELECTED' />
+              }
+
+              if (Object.keys(countriesAvailable).includes(country)) {
+                return <Country geo={geo} type='AVAILABLE' handleClick={handleClick} />
+              }
+
+              return <Country geo={geo} type='UNAVAILABLE' />
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+    </div >
   );
 };
 
@@ -18,61 +201,13 @@ const Description = () => {
     <>
       <Map />
 
-      <div className={InformationStyles.info}>
+      {/* <div className={InformationStyles.info}>
         <div><Success countryName="Polska" countryCode="POL" /></div>
         <div><Success countryName="Niemcy" countryCode="DEU" /></div>
         <div><Success countryName="Francja" countryCode="FRA" /></div>
         <div><Success countryName="Wielka Brytania" countryCode="GBR" /></div>
         <div><Success countryName="Stany Zjednoczone" countryCode="USA" /></div>
-      </div>
-
-      {/* <div className={InformationStyles.info}>
-        <div className={InformationStyles.country}>
-          <h4 className={InformationStyles.h4Bold}>
-            Rzeczpospolita Republika Polska
-          </h4>
-        </div>
-        <div className={InformationStyles.flag}>
-          <div className={InformationStyles.flagContent}>
-            <img
-              className={InformationStyles.flagImg}
-              src="https://i.ibb.co/xSYDqcG/zdjeciepolska.png"
-            />
-          </div>
-        </div>
-        <div className={InformationStyles.descriptionContent}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non
-          ultricies nibh. Suspendisse imperdiet, lacus eu congue gravida, mi
-          nisi fermentum magna, eu faucibus lorem nunc nec erat. In imperdiet,
-          purus at aliquet viverra, libero purus molestie mauris, ut consequat
-          ligula urna nec enim. Class aptent taciti sociosqu ad litora
-          torquent per conubia nostra, per inceptos himenaeos. Vestibulum nec
-          consequat lorem. Curabitur ultricies, dolor vel mattis luctus,
-          mauris mauris condimentum libero, sit amet rutrum ipsum nulla id
-          urna. Aenean ultricies, risus et tempus scelerisque, dolor justo
-          vulputate erat, nec feugiat turpis est in turpis.
-        </div>
-        <div className={InformationStyles.documentFront}>
-          <div className={InformationStyles.box} />
-        </div>
-
-        <div className={InformationStyles.frontText}>
-          <div className={InformationStyles.textItem}>
-            <h3 className={InformationStyles.h3Light}>Front wzoru dokumentu</h3>
-          </div>
-        </div>
-
-        <div className={InformationStyles.documentBack}>
-          <div className={InformationStyles.box} />
-        </div>
-
-        <div className={InformationStyles.backText}>
-          <div className={InformationStyles.textItem}>
-            <h3 className={InformationStyles.h3Light}>Tył wzoru dokumentu</h3>
-          </div>
-        </div>
       </div> */}
-
     </>
   );
 };
